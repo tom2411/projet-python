9fff90e3a7a4f47cda2862c097780a736494d0c6
Ribardiere Tom <tom.ribardiere@free.fr>
2018-01-08 14:42:09 +0100

    implémentation des fonctions vuent en TD Algo

--- labyrinthe/versionClassique/matrice.py
+++ labyrinthe/versionClassique/matrice.py
@@ -4,29 +4,40 @@
 
 def Matrice(nbLignes,nbColonnes,valeurParDefaut=0):
     """
-    crée une matrice de nbLignes lignes sur nbColonnes colonnes en mettant 
+    crée une matrice de nbLignes lignes sur nbColonnes colonnes en mettant
     valeurParDefaut dans chacune des cases
-    paramètres: 
+    paramètres:
       nbLignes un entier strictement positif qui indique le nombre de lignes
       nbColonnes un entier strictement positif qui indique le nombre de colonnes
       valeurParDefaut la valeur par défaut
     résultat la matrice ayant les bonnes propriétés
     """
-    pass
+    matrice=[]
+    sousListe=[]
+    for i in range(nbLignes):
+        matrice.append([valeurParDefaut]*nbColonnes)
+    return matrice
+
+assert Matrice(3,3,0)==[[0,0,0],[0,0,0],[0,0,0]]
+
 
 def getNbLignes(matrice):
     """
     retourne le nombre de lignes de la matrice
     paramètre: matrice la matrice considérée
     """
-    pass
+    return len(matrice)
+
+assert getNbLignes([[0,0,0],[0,0,0],[0,0,0]])==3
 
 def getNbColonnes(matrice):
     """
     retourne le nombre de colonnes de la matrice
     paramètre: matrice la matrice considérée
     """
-    pass
+    return len(matrice[0])
+
+assert getNbColonnes([[0,0,0],[0,0,0],[0,0,0]])==3
 
 def getVal(matrice,ligne,colonne):
     """
@@ -35,7 +46,9 @@ def getVal(matrice,ligne,colonne):
                 ligne le numéro de la ligne (en commençant par 0)
                 colonne le numéro de la colonne (en commençant par 0)
     """
-    pass
+    return matrice[ligne][colonne]
+
+assert getVal([[0,0,0],[0,0,0],[0,0,9]],2,2)==9
 
 def setVal(matrice,ligne,colonne,valeur):
     """
@@ -46,10 +59,14 @@ def setVal(matrice,ligne,colonne,valeur):
                 valeur la valeur à stocker dans la matrice
     cette fonction ne retourne rien mais modifie la matrice
     """
-    pass
+    matrice[ligne][colonne]=valeur
+    res=matrice
+    return res
+
+assert setVal([[0,0,0],[0,0,0],[0,0,9]],2,2,5)==[[0,0,0],[0,0,0],[0,0,5]]
 
 
-#------------------------------------------        
+#------------------------------------------
 # decalages
 #------------------------------------------
 def decalageLigneAGauche(matrice, numLig, nouvelleValeur=0):
@@ -95,4 +112,3 @@ def decalageColonneEnBas(matrice, numCol, nouvelleValeur=0):
     résultat: la valeur de la case "ejectée" par le décalage
     """
     pass
-
